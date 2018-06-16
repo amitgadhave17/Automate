cfbd25a1502f72d49bbeb2592609a041d4a4b1f3
Amit Gadhave <amitgadhave17@gmail.com>
2018-06-16 13:43:42 +0530

    Here

--- Logger.py
+++ Logger.py
@@ -6,14 +6,40 @@ import csv
 import sys, os, time
 from datetime import datetime
 
+def getDayMonth():
+	tday = datetime.today().day
+	tmon = datetime.today().strftime("%B")
+	return tday, tmon
 
-tday = datetime.today().day
-tmon = datetime.today().strftime("%B")
+def insertRecord():
+	try:
+		with open("Database/" + tmon + ".csv", "wb") as CSVfile:
+			CSVReader = csv.reader(CSVfile, delimiter = ',')
+	except:
+		print ("something is not right=")
 
-try:
-	with open("Database/" + tmon + ".csv", "wb") as CSVfile:
-		CSVReader = csv.reader(CSVfile, delimiter = ',')
 
-		pass
-except:
-	print ("something is not right=")
+if __name__=="__main__":
+	line1 = input("Sample_Count | a-SML/b-SMTS/c-Something_else | Description\n")
+	print(line1)
+	listline = line1.split(" ")
+	p
+	try:
+		sampleCount = int(listline[0])
+		detType = "OTHER"
+		if listline[1] == 'a' or listline[1] == 'A':
+			detectionType = "SML"
+		elif listline[1] == 'B' or listline[1] == 'B':
+			detectionType = "SMTS"
+		desc = " ".join(listline[1:])
+		print sampleCount
+		print detType
+		print desc
+	except:
+		print("Wrong Input")
+
+	if len(sys.argv) == 2:
+		print(sys.argv[1])
+	else:
+		# inserting record today
+		print(line)
